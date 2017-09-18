8098ca4d19aab582045c3befa868ca7324c93f97
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-18 09:58:54 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+//        sout = new line
+        System.out.print("Hello again");
+
+    }
+
+}
