5742bf8b1dd18ecb1d52edf5778ad39fd0b39d67
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-20 16:02:06 -0500

    lackadaisical Bob program

---
+++ src/Bob.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        String another;
+
+        do {
+            Scanner bob = new Scanner(System.in);
+            System.out.println("Hi Im Bob, What do you want?");
+
+            String userQuestion = bob.nextLine();
+            if (userQuestion.isEmpty()){
+                System.out.println("fine be that way");
+
+            } else if (userQuestion.endsWith("?")) {
+                System.out.println("Sure!!!");
+
+            } else if (userQuestion.equals(userQuestion.toUpperCase())) {
+                System.out.println("Whoa, chill out!");
+
+            }else
+                System.out.println("Whatever");
+
+            System.out.println("do you want to continue?");
+            another = bob.next();
+        }while (another.equalsIgnoreCase("yes"));
+    }
+
+}
+
+
+
+
