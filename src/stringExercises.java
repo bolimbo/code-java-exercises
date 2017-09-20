4089625f750a8f000e14681932fb2898150aa5f0
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-20 12:24:48 -0500

    string exercises done one variable many values

---
+++ src/stringExercises.java
@@ -0,0 +1,14 @@
+public class stringExercises {
+    public static void main(String[] args) {
+        String message = "i am the best";
+        System.out.println(message);
+        message = "my classmates suck";
+        System.out.println(message);
+        message = "blablabla";
+        System.out.println(message);
+        message = "hello codeup".toUpperCase();
+        System.out.println(message + "\n" + "this is a new line" + "\n" + "and anotha one");
+        message = "i go to code up";
+        System.out.println(message +"\\" + "cal");
+    }
+}
