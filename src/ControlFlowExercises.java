710b70a6733cb571394cb4a9c9506c1597e173cc
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-19 19:44:39 -0500

    control flow exercises finished

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -37,20 +37,20 @@ public class ControlFlowExercises {
 //    }
 //}
 
-Scanner scan = new Scanner(System.in);
-System.out.println("Enter a number: ");
-        System.out.println ("Number   Square   Cube");
-        System.out.printf ("%d        %d        %d\n", 0, (0 * 0), (0 * 0 * 0));
-        System.out.printf ("%d        %d        %d\n", 1, (1 * 1), (1 * 1 * 1));
-        System.out.printf ("%d        %d        %d\n", 2, (2 * 2), (2 * 2 * 2));
-        System.out.printf ("%d        %d        %d\n", 3, (3 * 3), (3 * 3 * 3));
-        System.out.printf ("%d        %d       %d\n", 4, (4 * 4), (4 * 4 * 4));
-        System.out.printf ("%d        %d       %d\n", 5, (5 * 5), (5 * 5 * 5));
-        System.out.printf ("%d        %d       %d\n", 6, (6 * 6), (6 * 6 * 6));
-        System.out.printf ("%d        %d       %d\n", 7, (7 * 7), (7 * 7 * 7));
-        System.out.printf ("%d        %d       %d\n", 8, (8 * 8), (8 * 8 * 8));
-        System.out.printf ("%d        %d       %d\n", 9, (9 * 9), (9 * 9 * 9));
-        System.out.printf ("%d       %d      %d\n", 10, (10 * 10), (10 * 10 * 10));
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        System.out.println("Number   Square   Cube");
+        System.out.printf("%d        %d        %d\n", 0, (0 * 0), (0 * 0 * 0));
+        System.out.printf("%d        %d        %d\n", 1, (1 * 1), (1 * 1 * 1));
+        System.out.printf("%d        %d        %d\n", 2, (2 * 2), (2 * 2 * 2));
+        System.out.printf("%d        %d        %d\n", 3, (3 * 3), (3 * 3 * 3));
+        System.out.printf("%d        %d       %d\n", 4, (4 * 4), (4 * 4 * 4));
+        System.out.printf("%d        %d       %d\n", 5, (5 * 5), (5 * 5 * 5));
+        System.out.printf("%d        %d       %d\n", 6, (6 * 6), (6 * 6 * 6));
+        System.out.printf("%d        %d       %d\n", 7, (7 * 7), (7 * 7 * 7));
+        System.out.printf("%d        %d       %d\n", 8, (8 * 8), (8 * 8 * 8));
+        System.out.printf("%d        %d       %d\n", 9, (9 * 9), (9 * 9 * 9));
+        System.out.printf("%d       %d      %d\n", 10, (10 * 10), (10 * 10 * 10));
 
 
         Scanner grade = new Scanner(System.in);
